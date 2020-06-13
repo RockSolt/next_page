1e29d6b242eeb1bc0e7278c873bf0bb1b39ff296
Todd Kummer <todd@rockridgesolutions.com>
2020-06-13 13:28:17 -0700

    Enable override of count query or count attributes

--- spec/next_page/pagination_attributes_spec.rb
+++ spec/next_page/pagination_attributes_spec.rb
@@ -32,4 +32,39 @@ RSpec.describe NextPage::PaginationAttributes do
       expect(instance.total_pages).to eq 4
     end
   end
+
+  context 'with count_query' do
+    let(:relation) { Jersey.offset(3).limit(5) }
+    before { instance.count_query = Jersey.limit(6) }
+
+    it '#total_count' do
+      expect(instance.total_count).to eq 20
+    end
+
+    it '#total_pages' do
+      expect(instance.total_pages).to eq 4
+    end
+  end
+
+  context 'with manually provided attributes' do
+    let(:relation) { Jersey.offset(3).limit(5) }
+
+    before do
+      instance.current_page = 10
+      instance.total_count = 123
+      instance.per_page = 4
+    end
+
+    it '#current_page' do
+      expect(instance.current_page).to eq 10
+    end
+
+    it '#next_page' do
+      expect(instance.next_page).to eq 11
+    end
+
+    it '#total_pages' do
+      expect(instance.total_pages).to eq 31
+    end
+  end
 end
